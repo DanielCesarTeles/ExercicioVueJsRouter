ea0bc702069a0f9808c13c2c55136f3fdb78f6de
DanielCesarTeles <daniel.cesarteles@gmail.com>
2020-06-26 23:24:34 -0300

    First commit ExercicioVueJsRouter

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Menu />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: { Menu}
+}
+</script>
+
+<style>
+a{
+  margin: 10px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <nav>
+      <a href="\home">Home</a>
+      <a href="\about">About</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Menu',
+  components: {}
+}
+</script>
+
+<style>
+
+a{
+  margin: 10px;
+}
+
+</style>
